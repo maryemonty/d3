e059bfdcc145230783757b63d3b991d00d2cc649
maryemonty <113999089+maryemonty@users.noreply.github.com>
2023-03-30 16:05:39 -1200

    changes

--- src/components/SingleBook.jsx
+++ src/components/SingleBook.jsx
@@ -2,24 +2,31 @@ import Card from 'react-bootstrap/Card';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import { Component } from 'react';
+import { render } from '@testing-library/react';
 
-function SingleBook(props) {
-    return (
-        <Container>
-            <Row>
-                <Col md='3' key={props.asin}>
-                    <Card className="d-flex">
-                        <Card.Body>
-                            <Card.Text>
-                                <Card.Img src={props.img} />
-                                <h3>{props.title}</h3>
-                            </Card.Text>
-                        </Card.Body>
-                    </Card>
-                </Col>
-            </Row>
-        </Container>
-    )
+class SingleBook extends Component {
+    state = {
+        selected: false
+    }
+    render() {
+        return (
+            <Container>
+                <Row>
+                    <Col md='3' key={this.props.asin}>
+                        <Card className="d-flex">
+                            <Card.Body>
+                                <Card.Text>
+                                    <Card.Img src={this.props.img} />
+                                    <h3>{this.props.title}</h3>
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
+            </Container>
+        )
+    }
 }
 
 export default SingleBook
